b2355d1ac614e3f90b3d58c9acfb64558a04ef98
FranMontesSegales <95092587+FranMontesSegales@users.noreply.github.com>
2023-02-08 20:23:05 +0100

    ACTUALISASION

--- Angular/Boletin2/angular/an/src/app/app-routing.module.ts
+++ Angular/Boletin2/angular/an/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { RecuperarComponent } from './recuperar/recuperar.component';
 import { RegisterComponent } from './register/register.component';
+import { VerificarComponent } from './verificar/verificar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'recuperar', component: RecuperarComponent },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'verificar', component: VerificarComponent }
+
 
 ];
 
--- Angular/Boletin2/angular/an/src/app/app.module.ts
+++ Angular/Boletin2/angular/an/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RecuperarComponent } from './recuperar/recuperar.component';
+import { VerificarComponent } from './verificar/verificar.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    RecuperarComponent,
+    VerificarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
